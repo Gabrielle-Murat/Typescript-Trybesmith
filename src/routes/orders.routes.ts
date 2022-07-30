55b48118a81c4b24216b107d1c122635b21d5d84
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-29 23:32:41 -0300

    feat: Adiciona rota para buscar pedidos

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import OrdersController from '../controllers/orders.controller';
 
 const router = Router();
+const ordersController = new OrdersController();
+
+router.get('/orders', ordersController.getAll);
 
 export default router;
