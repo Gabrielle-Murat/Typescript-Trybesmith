853f15449791eb9e2084d7e4f6772f56e271eee6
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-29 23:31:48 -0300

    feat: Cria função para buscar pedidos - Service

---
+++ src/services/orders.service.ts
@@ -0,0 +1,17 @@
+import connection from '../models/connection';
+import OrdersModel from '../models/orders.model';
+import Order from '../interfaces/order.interface';
+
+export default class OrdersService {
+  public model: OrdersModel;
+
+  constructor() {
+    this.model = new OrdersModel(connection);
+  }
+
+  public async getAll(): Promise<Order[]> {
+    const orders = await this.model.getAll();
+
+    return orders;
+  }
+}
