dfc30e04f28b3e88997fbf1176db14215e27630d
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 15:42:37 -0300

    feat: Adiciona validação de produtos na rota

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import productValidation from '../middlewares/product.validation';
 
 const router = Router();
 const productsController = new ProductsController();
 
-router.post('/products', productsController.create);
+router.post('/products', productValidation, productsController.create);
 router.get('/products', productsController.getAll);
 
 export default router;
