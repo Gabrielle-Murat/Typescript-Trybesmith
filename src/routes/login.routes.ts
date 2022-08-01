eb10844f318c6f23ea23c17a9806d1132e2c7748
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 15:26:00 -0300

    feat: Adiciona rota para fazer login
    Endpoint do tipo POST

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import LoginController from '../controllers/login.controller';
+import loginValidation from '../middlewares/login.validation';
 
 const router = Router();
+const loginController = new LoginController();
+
+router.post('/login', loginValidation, loginController.userLogin);
 
 export default router;
