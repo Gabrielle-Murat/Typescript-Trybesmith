768aa3982b8e2c6465face9c7e76835dfd4313b3
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 15:53:41 -0300

    feat: Adiciona validação de users na rota

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
+import userValidation from '../middlewares/user.validation';
 
 const router = Router();
 const usersController = new UsersController();
 
-router.post('/users', usersController.create);
+router.post('/users', userValidation, usersController.create);
 
 export default router;
