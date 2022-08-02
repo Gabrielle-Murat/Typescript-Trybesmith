6d9900b0806a969dfa75f7e508a8faaad684a50a
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 21:26:55 -0300

    feat: Cria função para cadastrar pedidos
    Além da função para criação de novos pedidos, criei uma função para atualizar a tabela de produtos que tem o id do pedido como foreign key

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import Order from '../interfaces/order.interface';
 
 export default class OrdersModel {
@@ -26,5 +26,27 @@ export default class OrdersModel {
     `);
     const [rows] = result;
     return rows as Order[];
+  }
+
+  public async create(userId: number): Promise<number> {
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+      [userId],
+    );
+    const [orderInserted] = result;
+    const { insertId } = orderInserted;
+    return insertId;
+  }
+
+  public async updateProductsTable(orderId: number, productId: number): Promise<number> {
+    await this.connection.execute(
+      `
+        UPDATE Trybesmith.Products
+        SET orderId = ?
+        WHERE id = ?;
+      `,
+      [orderId, productId],
+    );
+    return productId;
   }
 }
