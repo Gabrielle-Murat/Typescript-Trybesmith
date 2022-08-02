a3494482f223963d48b6c7a694db1d7f8948417b
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 21:35:51 -0300

    feat: Adiciona validações à rota de pedidos
    Validações de token e de corpo da requisição

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders.controller';
+import tokenValidation from '../middlewares/auth/token.validation';
+import orderValidation from '../middlewares/order.validation';
 
 const router = Router();
 const ordersController = new OrdersController();
 
 router.get('/orders', ordersController.getAll);
+router.post('/orders', tokenValidation, orderValidation, ordersController.create);
 
 export default router;
