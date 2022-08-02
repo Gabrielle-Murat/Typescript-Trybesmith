2d7a3e200c80fe11ee3fae41a1903f70d65e3ac0
Gabrielle-Murat <belle.murat@gmail.com>
2022-08-01 21:29:25 -0300

    feat: Cria função para cadastrar pedidos - Controller

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -8,4 +8,14 @@ export default class OrdersController {
     const orders = await this.ordersService.getAll();
     res.status(200).json(orders);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const { productsIds } = req.body;
+
+    // variável advinda do middleware anterior (no caso, o tokenValidation)
+    const userId = res.locals.id;
+
+    const response = await this.ordersService.create(userId, productsIds);
+    return res.status(201).json(response);
+  };
 }
